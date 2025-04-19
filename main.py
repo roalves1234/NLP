496f382030ac39b51ae5f414483c49d6691f1451
roalves1234 <roalves1234@hotmail.com>
2025-04-19 18:58:31 -0300

    Acurácia

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from transformers import AutoTokenizer, AutoModel
 from funcoes import preprocess_text, get_bert_embeddings
 
 print("> Carregamento do dataset")
-data_frame = pandas.read_csv("FolhaArticles_half.csv", sep="\t") # Baixe o dataset em https://www.kaggle.com/datasets/luisfcaldeira/folha-news-of-the-brazilian-newspaper-2024
+data_frame = pandas.read_csv("FolhaArticles_thin.csv", sep="\t") # Baixe o dataset em https://www.kaggle.com/datasets/luisfcaldeira/folha-news-of-the-brazilian-newspaper-2024
 
 print("> Pré-processamento do texto")
 data_frame['processed_text'] = data_frame['Title'].apply(preprocess_text)
 
-print("> Garantia de que a coluna 'categories' tenha um tipo consisstente")
+print("> Garantia de que a coluna 'categories' tenha um tipo consistente")
 data_frame['categories'] = data_frame['categories'].astype(str)
 
 print("> Divisão em treino e teste")
