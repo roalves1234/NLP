b2a25511ee188990f62201ddcd718266b2dfffe6
roalves1234 <roalves1234@hotmail.com>
2025-04-21 11:16:40 -0300

    Comentários

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from transformers import AutoTokenizer, AutoModel
 from funcoes import preprocess_text, get_bert_embeddings
 
 print("> Carregamento do dataset")
-data_frame = pandas.read_csv("FolhaArticles_thin.csv", sep="\t") # Baixe o dataset em https://www.kaggle.com/datasets/luisfcaldeira/folha-news-of-the-brazilian-newspaper-2024
+data_frame = pandas.read_csv("FolhaArticles.csv", sep="\t") # Baixe o dataset em https://www.kaggle.com/datasets/luisfcaldeira/folha-news-of-the-brazilian-newspaper-2024
 
 print("> Pré-processamento do texto")
 data_frame['processed_text'] = data_frame['Title'].apply(preprocess_text)
@@ -15,7 +15,7 @@ print("> Garantia de que a coluna 'categories' tenha um tipo consistente")
 data_frame['categories'] = data_frame['categories'].astype(str)
 
 print("> Divisão em treino e teste")
-X_train, X_test, y_train, y_test = train_test_split(
+X_train, X_test, y_train, y_test = train_test_split( # X = processed_text, y = categories
     data_frame['processed_text'], data_frame['categories'], test_size=0.2, random_state=42
 )
 
