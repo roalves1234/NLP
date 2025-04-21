5182d5d2837b05d3231da7f0c3dc19446c553f73
roalves1234 <roalves1234@hotmail.com>
2025-04-21 11:53:54 -0300

    Alteração de comentário para print

--- consume_knn_model.py
+++ consume_knn_model.py
@@ -1,26 +1,27 @@
+print("> Carregamento das bibliotecas")
 import pickle
 from transformers import AutoTokenizer, AutoModel
 from funcoes import preprocess_text, get_bert_embeddings
 
-# Carregar o modelo KNN
+print("> Carregar o modelo KNN")
 with open("modelo_knn.pkl", "rb") as file:
     knn_model = pickle.load(file)
 
-# Carregar o modelo BERTimbau
+print("> Carregar o modelo BERTimbau")
 tokenizer = AutoTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
 model = AutoModel.from_pretrained("neuralmind/bert-base-portuguese-cased")
 
-# Solicitar o título da notícia ao usuário
+print("> Solicitar o título da notícia ao usuário")
 title = input("Informe o título da notícia: ").strip()
 
-# Pré-processar o texto
+print("> Pré-processar o texto")
 processed_title = preprocess_text(title)
 
-# Gerar embeddings para o título
+print("> Gerar embeddings para o título")
 embeddings = get_bert_embeddings([processed_title], tokenizer, model)
 
-# Fazer a previsão da categoria
+print("> Fazer a previsão da categoria")
 predicted_category = knn_model.predict(embeddings)[0]
 
-# Exibir a categoria prevista
+print("> Exibir a categoria prevista")
 print(f"A categoria prevista para o título informado é: {predicted_category}")
