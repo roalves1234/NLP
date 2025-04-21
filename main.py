0c2866a17d98aada1d2469bfb51c94ebb076f910
roalves1234 <roalves1234@hotmail.com>
2025-04-21 11:48:43 -0300

    Introdução de mais um modelo - o KNN

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+print("> Carregamento das bibliotecas")
 import pandas as pandas
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from transformers import AutoTokenizer, AutoModel
 from funcoes import preprocess_text, get_bert_embeddings
+from sklearn.neighbors import KNeighborsClassifier
 
 print("> Carregamento do dataset")
 data_frame = pandas.read_csv("FolhaArticles.csv", sep="\t") # Baixe o dataset em https://www.kaggle.com/datasets/luisfcaldeira/folha-news-of-the-brazilian-newspaper-2024
@@ -28,8 +30,19 @@ X_train_embeddings = get_bert_embeddings(X_train.tolist(), tokenizer, model)
 print("> Geração de embeddings de teste")
 X_test_embeddings = get_bert_embeddings(X_test.tolist(), tokenizer, model)
 
+# Perguntar ao usuário qual classificador usar
+classifier_choice = input("Escolha o classificador (1-RandomForest ou 2-KNN): ").strip().lower()
+
+if classifier_choice == "1":
+    print("> Usando o classificador RandomForest")
+    classifier = RandomForestClassifier(random_state=42)
+elif classifier_choice == "2":
+    print("> Usando o classificador KNN")
+    classifier = KNeighborsClassifier()
+else:
+    raise ValueError("Classificador inválido. Escolha 'RandomForest' ou 'KNN'.")
+
 print("> Treinamento do classificador")
-classifier = RandomForestClassifier(random_state=42)
 classifier.fit(X_train_embeddings, y_train)
 
 print("> Avaliação do modelo")
