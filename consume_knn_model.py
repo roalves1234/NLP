99b7c7302986c377673329e94f1b81a3b4617c12
roalves1234 <roalves1234@hotmail.com>
2025-04-21 16:36:38 -0300

    Informações de download

--- consume_knn_model.py
+++ consume_knn_model.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer, AutoModel
 from funcoes import preprocess_text, get_bert_embeddings
 
 print("> Carregamento do modelo KNN")
-with open("modelo_knn.pkl", "rb") as file:
+with open("modelo_knn.pkl", "rb") as file: # fazer download em https://drive.google.com/file/d/1KYV7M7N9NpC_a7EAOxdYN3BV9uyjx7g7/view?usp=drive_link
     trained_model = pickle.load(file)
     trained_model = trained_model["classifier"]
     
